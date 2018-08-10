97b7bd5b378685bfb2ed9879b48f3db964664fee
lorenpabst@gmail.com <lorenpabst@gmail.com>
2018-08-09 20:37:17 -0600

    added some pagination buttons and code

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -15,17 +15,25 @@ class Home extends Component {
       showFullSizeOverlay: false,
       selectedImage: {},
       errorMessage: '',
+      resultsPage: 1,
+      numPages: 1,
+      backButtonActive: false,
+      nextButtonActive: false,
     }
 
     this.searchPhotos = this.searchPhotos.bind(this);
     this.openFullSizeOverlay = this.openFullSizeOverlay.bind(this);
     this.closeFullSizeOverlay = this.closeFullSizeOverlay.bind(this);
+    this.getNewPage = this.getNewPage.bind(this);
   }
 
-  searchPhotos(e){
-    e.preventDefault();
+  // if this is called with the newPageNum var it searches for that page of results, otherwise it gets page 1
+  searchPhotos(e, newPageNum){
+    if (e) e.preventDefault();
+
+    let pageNum = newPageNum || 1;
     
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${config.apiKey}&text=${this.state.userInput}&per_page=25&format=json&nojsoncallback=1`)
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${config.apiKey}&text=${this.state.userInput}&per_page=25&format=json&nojsoncallback=1&page=${pageNum}`)
     .then( res => {
       if (!res.data || !res.data.photos || !res.data.photos.photo || res.data.photos.photo.length === 0){
         return this.setState({
@@ -36,6 +44,10 @@ class Home extends Component {
 
       let errorMessage = '';
       let photos = res.data.photos.photo;
+      let resultsPage = res.data.photos.page;
+      let numPages = res.data.photos.pages;
+      let backButtonActive = resultsPage >= 2 ? true : false;
+      let nextButtonActive = resultsPage < numPages ? true : false;
 
       photos.forEach(photo => {
         photo.thumbnail = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_q.jpg`;
@@ -43,7 +55,7 @@ class Home extends Component {
         photo.shortTitle = photo.title.length < 28 ? photo.title : photo.title.substring(0, 25) + '...';
       })
 
-      this.setState({photos, errorMessage});
+      this.setState({photos, errorMessage, resultsPage, numPages, backButtonActive, nextButtonActive});
     })
   }
 
@@ -61,14 +73,29 @@ class Home extends Component {
     })
   }
 
+  // if the new page requested is within appropriate bounds, gets the new page of results
+  getNewPage(pageIncrementer){
+    let newPageNum = this.state.resultsPage + pageIncrementer;
+    if (newPageNum >= 1 && newPageNum <= this.state.numPages){
+      this.searchPhotos(null, newPageNum);
+    }
+  }
+
   render() {
+    let backButtonBackground = this.state.backButtonActive ? "#006dac" : "#ccc";
+    let nextButtonBackground = this.state.nextButtonActive ? "#006dac" : "#ccc";
+
     return (
       <div className="home">
 
         <p className='header'>Search Flickr For Photos</p>
         <form className='searchForm' onSubmit={(e) => this.searchPhotos(e)} >
           <input onChange={(e) => this.setState({userInput: e.target.value})} placeholder='enter search term' />
-          <div onClick={(e) => this.searchPhotos(e)} >Search</div>
+          <div className='searchFormControls'>
+            <div onClick={() => this.getNewPage(-1)} style={{background: backButtonBackground}} >Back</div>
+            <div onClick={(e) => this.searchPhotos(e)} >Search</div>
+            <div onClick={() => this.getNewPage(1)} style={{background: nextButtonBackground}} >Next</div>
+          </div>
         </form>
 
         <SearchResults photos={this.state.photos} openFullSizeOverlay={this.openFullSizeOverlay} />
